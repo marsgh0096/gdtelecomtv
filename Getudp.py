87142505f3be28f0d20e861b36964dcbf136d430
marsgh0096 <47437382+marsgh0096@users.noreply.github.com>
2024-07-02 17:43:34 +0800

    Add files via upload

---
+++ Getudp.py
@@ -0,0 +1,22 @@
+# @Time : 2024/7/2 17:35 02 17
+# @Author : Mars.G
+# @FileName: 获取第一个 IP.py
+# @Software: PyCharm
+
+import requests
+import re
+
+# 您的目标网址
+url = 'https://fofa.info/result?qbase64=IlVEUFhZIiAmJiByZWdpb249Ikh1bmFuIg%3D%3D'
+
+# 发起 GET 请求
+response = requests.get(url)
+
+# 获取网页 html 内容
+html_content = response.text
+
+# 通过正则在 HTML 中寻找 IP 地址，匹配IP地址可能带有的端口
+ip_address = re.findall( r'[0-9]+(?:\.[0-9]+){3}(?:\:[0-9]{1,5})?', html_content)
+
+# 打印出第一个发现的 IP 地址
+print("第一个IP地址: ", ip_address[0] if ip_address else "None")
